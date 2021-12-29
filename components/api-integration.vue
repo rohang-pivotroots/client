29354358ceff0f5f6febecdf400a4b7ff8240850
Rohan Gosavi <rohan.gosavi@pivotroots.com>
2021-12-29 12:32:20 +0530

    client

---
+++ components/api-integration.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row no-gutters">
+        <section class="userSection my-3">
+            <div>
+                <p class="name">Name : <span class="font-weight-bold">{{user.name}}</span></p>
+                <p class="username">Username : <span class="font-weight-bold">{{user.username}}</span></p>
+                <p class="emailid">Email : <span class="font-weight-bold">{{user.email}}</span></p>
+                <p class="phone">Phone : <span class="font-weight-bold">{{user.phone}}</span></p>
+                <p>Website : <NuxtLink :to="user.website" class="website"><span class="font-weight-bold">{{user.website}}</span></NuxtLink></p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+ export default{
+     props: {
+         user: {
+             type: Object,
+             default: () => {}
+         }
+     }
+ }
+</script>
+
+<style scoped>
+    .userSection{
+        font-size: 16px;
+        /* border: 1px solid #333; */
+        /* border-radius: 10px; */
+    }
+    .userSection p{
+        margin: 0;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div class="container ">
+    <h1 class="header">API Integraion</h1>
+    <p class="">This is a demo for api integration using Nuxt JS</p>
+    <!-- <button class="btn btn-primary">Add Data</button> -->
+    <section class="row no-gutters">
+      <api-integration class="col-4" v-for="user in users" :key="user.username" :user="user"/>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    this.users = await fetch(
+      'https://jsonplaceholder.typicode.com/users'
+    ).then((res) => res.json())
+  },
+  data() {
+    return {
+      users: []
+    }
+  }
+}
+</script>
